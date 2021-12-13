6b50d27263d441d1c252f6c60ed1d0daf2d2ffc0
Roman Komkov <r.komkov@gmail.com>
2021-12-13 14:40:24 +0100

    More and more Lua to nvim

---
+++ .config/nvim/lua/config/dap.lua
@@ -0,0 +1,27 @@
+require("dap")
+-- vim.fn.sign_define("DapBreakpoint", { text = "ﭦ ", texthl = "", linehl = "", numhl = "" })
+vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "LspDiagnosticsSignError", linehl = "", numhl = "" })
+-- vim.fn.sign_define("DapStopped", { text = " ", texthl = "", linehl = "", numhl = "" })
+
+require("dap-go").setup()
+require("telescope").load_extension("dap")
+require("nvim-dap-virtual-text").setup({
+	commented = true,
+})
+
+vim.api.nvim_set_keymap("n", "<leader>db", "<cmd>lua require'dap'.toggle_breakpoint()<cr>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>ds", "<cmd>lua require'dap'.continue()<cr>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>dc", "<cmd>lua require'dap'.continue()<cr>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>dd", "<cmd>lua require'dap'.disconnect()<cr>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>dC", "<cmd>lua require'dap'.run_to_cursor()<cr>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>di", "<cmd>lua require'dap'.step_into()<cr>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>du", "<cmd>lua require'dap'.step_out()<cr>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>do", "<cmd>lua require'dap'.step_over()<cr>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>dq", "<cmd>lua require'dap'.close()<cr>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>dh", "<cmd>lua require'dap.ui.widgets'.hover()<cr>", { noremap = true })
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>d?",
+	"<cmd>lua local widgets=require'dap.ui.widgets';widgets.centered_float(widgets.scopes)<cr>",
+	{ noremap = true }
+)
